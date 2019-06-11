af9fdc24a6bf366c54482a1a5cf180aeffb55917
tobiasfrick <hans.tobias.frick@gmail.com>
2019-06-11 17:59:49 +0200

    Add .travis.yml file

--- tests/run.sh
+++ tests/run.sh
@@ -2,4 +2,4 @@
 set -xeu pipefail
 
 rm -rf outdir
-bash DBSpro_automation.sh DBSpro-testdata-0.1/2-PrEST02_S11_L001_R1_001.fastq.gz outdir
+bash DBSpro_automation.sh testdata/2-PrEST02_S11_L001_R1_001.fastq.gz outdir
