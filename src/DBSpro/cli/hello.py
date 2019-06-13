64a906de14c35f3a5b972f764447086a7ac3c491
tobiasfrick <hans.tobias.frick@gmail.com>
2019-06-13 18:13:59 +0200

    Remove test scripts

--- src/DBSpro/cli/hello.py
+++
@@ -1,9 +0,0 @@
-"""
-Prints hello in a friendly manner.
-"""
-
-def main(args) -> None:
-    print(args.string)
-
-def add_arguments(parser):
-    parser.add_argument("string", type=str, help="string")
