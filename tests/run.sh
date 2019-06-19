7a35bde37eb40b59d6f5c7347219f013db7d56f0
tobiasfrick <hans.tobias.frick@gmail.com>
2019-06-19 17:21:19 +0200

    Add md5sum comparison

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,7 @@
 #!bin/bash
-set -xeu pipefail
+set -xoeu pipefail
 
 rm -rf outdir
 bash DBSpro_automation.sh testdata/reads.10k-DBS.fastq.gz outdir
+m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
+test $m == 3c086da27caec5dd08d0da695b81b067
