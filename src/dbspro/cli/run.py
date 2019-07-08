2b6cad74b8eadfe4acc4dc74777fd31e1615d820
Pontus Hojer <pontushojer@gmail.com>
2019-07-08 11:40:37 +0200

    Included conditionals for checking input files and output directory.

--- src/dbspro/cli/run.py
+++ src/dbspro/cli/run.py
@@ -4,7 +4,7 @@ Run DPS-Pro pipeline
 
 import logging
 import os
-import shutil
+from pathlib import Path
 import sys
 import pkg_resources
 from snakemake import snakemake
@@ -13,19 +13,53 @@ from dbspro.utils import available_cpu_count
 
 logger = logging.getLogger(__name__)
 
+accepted_file = 'reads.fastq.gz'
+accepted_file_ext = '.fastq.gz'
+
 
 def main(args):
+
+    cwd = Path(os.getcwd())
+
+    # Check if path to output directory is absolute or make it so.
+    if not os.path.isabs(args.directory):
+        args.directory = cwd / args.directory
+
+    # Check if directory exists. If not make one.
+    if not os.path.isdir(args.directory):
+        os.mkdir(args.directory)
+        logging.info(f'Output directory {args.directory} created.')
+
+    # If no targets --> run whole pipeline i.e. use final file names.
     if not args.targets:
         args.targets = ['umi-counts.txt', 'umi-density-plot.png', 'read-density-plot.png']
 
-    if os.path.isdir(args.directory):
-        if args.force and input(f'Directory {args.directory} exists. Remove? (y/n)') == 'y':
-            shutil.rmtree(args.directory)
-    else:
-        os.mkdir(args.directory)
+    # Append full path to targets.
+    targets_with_path = [args.directory / t for t in args.targets]
 
-    targets_with_path = [f'{args.directory}/{t}' for t in args.targets]
-    # Modified from: https://github.com/NBISweden/IgDiscover/
+    # Check if correct file exists in output directory.
+    # If not, try using the input fastq argument if given.
+    if os.path.isfile(args.directory / accepted_file):
+        logging.info(f'Found correct file in given directory, continuing analysis.')
+    elif args.fastq:
+        # Check if path to output directory is absolute or make it so.
+        if not os.path.isabs(args.fastq):
+            args.fastq = cwd / args.fastq
+
+        # Check if file exists.
+        if not os.path.isfile(args.fastq):
+            raise FileNotFoundError(f'File {args.fastq} not found.')
+
+        # Check if file has the correct extension
+        if not str(args.fastq).endswith(accepted_file_ext):
+            raise FileNotFoundError(f'File {args.fastq} is not accepted input.')
+
+        # Create symbolic link to file in output directory.
+        os.symlink(args.fastq, args.directory / accepted_file)
+    else:
+        raise FileNotFoundError(f'Required file not found in folder or given as input.')
+
+    # Lines below are modified from: https://github.com/NBISweden/IgDiscover/
     snakefile_path = pkg_resources.resource_filename('dbspro', 'Snakefile')
     logger.root.handlers = []
     success = snakemake(snakefile_path,
@@ -46,8 +80,8 @@ def add_arguments(parser):
                         help="Maximum number of cores to run in parallel. Default: Use as manny as available.")
     parser.add_argument('targets', nargs='*', default=[],
                         help='File(s) to create (without paths). If omitted, the full pipeline is run.')
-    parser.add_argument('-d', '--directory',
-                        help='Path to directory in which to run pipeline and store output. '
-                             'Should contain input fastq file (or symbolic link to file).')
-    parser.add_argument('-f', '--force', default=False, action='store_true',
-                        help='Force to run analysis, removes existing analysis if present.')
+    parser.add_argument('-d', '--directory', default=os.getcwd(), type=Path,
+                        help='Path to directory in which to run pipeline and store output. Should contain input '
+                             'fastq file (or symbolic link to file) unless given as argument. Default: CWD')
+    parser.add_argument('-f', '--fastq', default=None, type=str,
+                        help="Input fastq file. Should have extension '.fastq.gz'. Default: None")
