70932a053e5231bb80c5776206632047e6686d6a
Pontus Hojer <pontushojer@gmail.com>
2019-07-09 10:49:14 +0200

    Error search

--- tests/run.sh
+++ tests/run.sh
@@ -3,5 +3,6 @@ set -xoeu pipefail
 
 rm -rf outdir
 bash DBS-Pro_automation.sh testdata/reads-10k-DBS.fastq.gz outdir
+head outdir/umi-counts.txt
 m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
 test $m == 7cc629068f91a8f635a9c1e557d404e5
