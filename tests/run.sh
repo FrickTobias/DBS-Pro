aea25ad157380ee2c508e9cf71ccb9a5fd444698
Pontus Hojer <pontushojer@gmail.com>
2019-07-17 16:35:06 +0200

    Edits based on review. Changed tsv files to positional as they are now required.

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,8 @@ set -xoeu pipefail
 
 rm -rf outdir
 
-dbspro run -d outdir -f testdata/reads-10k-DBS.fastq.gz -hf construct-info/handles.tsv -af construct-info/ABC-sequences.tsv
+dbspro run -d outdir -f testdata/reads-10k-DBS.fastq.gz \
+    construct-info/handles.tsv construct-info/ABC-sequences.tsv
 
 m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
 test $m == 5f64eccd8b8e67d4aa954bcceb359b70
