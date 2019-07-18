09d657f74a8873abda47c4175515a97157c815ea
Pontus Hojer <pontushojer@gmail.com>
2019-07-18 11:05:38 +0200

    Updated md5sum as header in umi-counts.txt has been changed.

--- tests/run.sh
+++ tests/run.sh
@@ -6,4 +6,4 @@ rm -rf outdir
 dbspro run -d outdir -f testdata/reads-10k-DBS.fastq.gz -hf construct-info/handles.tsv -af construct-info/ABC-sequences.tsv
 
 m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
-test $m == 5f64eccd8b8e67d4aa954bcceb359b70
+test $m == d7e4d6d91069a97e2fd2cc3945222167
