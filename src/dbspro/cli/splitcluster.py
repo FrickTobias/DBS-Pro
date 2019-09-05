0b0755abc05391de6a1c81fa5fbd2a7c82867ece
Pontus Hojer <pontushojer@gmail.com>
2019-09-05 15:39:40 +0200

    Cleanup of code, restructuring and added comments.

--- src/dbspro/cli/splitcluster.py
+++ src/dbspro/cli/splitcluster.py
@@ -4,7 +4,6 @@ Split ABC file based on DBS cluster and cluster UMIs for each partion using UMI-
 import dnaio
 import argparse
 from umi_tools import UMIClusterer
-import pandas as pd
 from collections import Counter, defaultdict
 import sys
 import time
@@ -12,64 +11,69 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+# Counter object for statistics.
+stats = Counter()
+
+
+def get_umis(file, length=0):
+    umis = dict()
+    for read in file:
+        if length == len(read.sequence):
+            umis[read.name] = read.sequence
+        else:
+            stats['Reads filtered out'] += 1
+    return umis
+
+
+def assign_to_dbs(file, umis):
+    """
+    Create structure for storing DBS, UMI and read name
+
+           {DBS1:{UMI1:[name1, name2, ...],
+                  UMI2:[name3, ...],
+                  ...
+                  }
+            DBS2:{UMI1:[name1, name2, ...],
+                  UMI2:[name3, ...],
+                  ...
+                  }
+           }
+    """
+    dbs_umis = defaultdict(dict)
+    for read in file:
+        # Skip reads that are not to be clustered
+        if read.name not in umis:
+            continue
+
+        # Get sequences
+        dbs = read.sequence
+        umi = umis[read.name]
+
+        try:
+            dbs_umis[dbs][umi].append(read.name)
+        except KeyError:
+            dbs_umis[dbs][umi] = [read.name]
+
+        stats["Reads kept"] += 1
+    return dbs_umis
+
 
 def main(args):
-    for item in vars(args).items():
-        print(item)
-
     logger.info(f"Filtering reads not of length {args.length} bp.")
-
-    stats = Counter()
 
     time_start = time.time()
 
     # Read ABC fasta with UMI sequences and save read name and sequence.
-    umis = dict()
     with dnaio.open(args.abcfile, mode="r") as file:
-        for read in file:
-            if args.length == len(read.sequence):
-                seq = bytes(read.sequence, encoding='utf-8')
-                umis[read.name] = seq
-            else:
-                stats['Reads filtered out'] += 1
-
-    logger.info(f"Reads filtered out: {stats['Reads filtered out']:,}")
+        umis = get_umis(file, length=args.length)
 
     time_filtered = time.time()
     logger.info(f"Time for filtering: {time_filtered - time_start} s")
     logger.info(f"Assigning UMIs to DBS clusters")
-    # Create structure for storing DBS, UMI and read name
-    #
-    #      {DBS1:{UMI1:[name1, name2, ...],
-    #             UMI2:[name3, ...],
-    #             ...
-    #             }
-    #       DBS2:{UMI1:[name1, name2, ...],
-    #             UMI2:[name3, ...],
-    #             ...
-    #             }
-    #      }
-    dbs_umis = defaultdict(dict)
+
     with dnaio.open(args.dbsfile, mode="r") as file:
-        for read in file:
-            # Skip reads that are not to be clustered
-            if read.name not in umis:
-                continue
+        dbs_umis = assign_to_dbs(file, umis)
 
-            # Get sequences
-            dbs = read.sequence
-            umi = umis[read.name]
-
-
-
-            try:
-                dbs_umis[dbs][umi].append(read.name)
-            except KeyError:
-                dbs_umis[dbs][umi] = [read.name]
-
-            stats["Reads kept"] += 1
-
-    logger.info(f"Reads kept: {stats['Reads kept']}")
     logger.info(f"DBS clusters linked to ABC: {len(dbs_umis)}")
 
     time_assign = time.time()
@@ -82,38 +86,35 @@ def main(args):
 
     with dnaio.open(args.output, fileformat="fasta", mode="w") as output:
         for dbs, umis in dbs_umis.items():
-
-            counts = {umi: len(reads) for umi, reads in umis.items()}
+            # Encode each UMI for UMITools and perpare counts
+            counts = {bytes(umi, encoding='utf-8'): len(reads) for umi, reads in umis.items()}
 
             stats["Total UMIs"] += len(counts)
 
-            clustered_umis = umitools_clustering(counts, clusterer, threshold=args.threshold)
+            # Cluster umis
+            clustered_umis = clusterer(counts, threshold=args.threshold)
 
             stats["Total clustered UMIs"] += len(clustered_umis)
 
+            # Loop over clusters and write reads with corrected UMI.
             for cluster in clustered_umis:
                 seqs = [seq.decode("utf-8") for seq in cluster]
                 canonical_sequnce = seqs[0]
 
-                for seq in cluster:
+                for seq in seqs:
                     for read_name in umis[seq]:
                         read = dnaio.Sequence(read_name, canonical_sequnce)
                         output.write(read)
 
     time_end = time.time()
     logger.info(f"Time for clustering: {time_end - time_assign} s")
-    logger.info(f"Total UMIs: {stats['Total UMIs']}")
-    logger.info(f"Total clustered UMIs: {stats['Total clustered UMIs']}")
-
     logger.info(f"Total time to run: {time_end-time_start} s")
 
-
-def umitools_clustering(umi_counts, clusterer, threshold=None):
-    try:
-        return clusterer(umi_counts, threshold=threshold)
-    except ValueError as e:
-        print(umi_counts)
-        sys.exit(e)
+    # Send stats to log
+    logger.info(f"Reads filtered out: {stats['Reads filtered out']:,}")
+    logger.info(f"Reads kept: {stats['Reads kept']}")
+    logger.info(f"Total UMIs: {stats['Total UMIs']}")
+    logger.info(f"Total clustered UMIs: {stats['Total clustered UMIs']}")
 
 
 def add_arguments(parser):
@@ -131,5 +132,3 @@ def add_arguments(parser):
     parser.add_argument("-m", "--method", type=str, choices=["unique", "percentile", "cluster", "adjacency",
                                                              "directional"],
                         help="Select clustering method. DEFAULT=directional")
-
-
