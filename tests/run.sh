bc4a3852f6a4edf36baa456bba4e93d1950e958c
Pontus Hojer <pontushojer@gmail.com>
2019-11-05 16:15:27 +0100

    Move plotting analysis from analysis.py script to a Jupyter Notebook. The `report_template.ipnyb` is copied into the analysis folder and run to create a `report.ipynb` notebook for further analysis and a shareable `report.html` file.

--- tests/run.sh
+++ tests/run.sh
@@ -6,5 +6,5 @@ rm -rf outdir
 dbspro run -d outdir -f testdata/reads-10k-DBS.fastq.gz \
     ../construct-info/handles.tsv ../construct-info/ABC-sequences.fasta
 
-m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
+m=$(cat outdir/umi_counts.tsv | md5sum | cut -f 1 -d" ")
 test $m == 62ce11e3ac4fb7428a6bf4bfed84f529
