5ba72b7f8d26638be2557619c3c9bd99e7e481bf
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-08 14:43:31 +0100

    Finish init, now writes - abc.fa - .yaml (config file) - (links) reads.fa to outdir

--- src/dbspro/cli/init.py
+++ src/dbspro/cli/init.py
@@ -5,40 +5,49 @@ import logging
 import os
 import os.path
 import sys
+import dnaio
 from pathlib import Path
 from importlib_resources import read_binary
 
 logger = logging.getLogger(__name__)
 
 CONFIGURATION_FILE_NAME = "dbspro.yaml"
+ABC_FILE_NAME = "ABC-sequences.fa"
 
 
 def add_arguments(parser):
     parser.add_argument("reads", type=Path, help="Read file (.fastq.gz)")
     parser.add_argument("directory", type=Path, help="New analysis directory to create")
+    parser.add_argument("--abc", default="../construct-info/ABC-sequnces.fasta", type=str,
+                        metavar="ABC-sequences.fasta",
+                        help="Antibody barcode sequence (ABC) sequnce fasta file")
+
+    return parser
 
 
 def main(args):
-    init(args.directory, args.reads)
+    init(args.directory, args.reads, args.abc)
 
 
-def init(directory: Path, reads1: Path):
+def init(directory: Path, reads: Path, abc: str):
     if " " in str(directory):
         logger.error("The name of the analysis directory must not contain spaces")
         sys.exit(1)
 
-    create_and_populate_analysis_directory(directory, reads1)
+    create_and_populate_analysis_directory(directory, reads, abc)
 
     logger.info(f"Directory {directory} initialized.")
     logger.info(
-        'Edit %s/%s, then run "cd %s && blr run" to start the analysis',
+        'Edit %s/%s and %s/%s, then run "cd %s && dbspro run" to start the analysis',
         directory,
         CONFIGURATION_FILE_NAME,
+        directory,
+        ABC_FILE_NAME,
         directory,
     )
 
 
-def create_and_populate_analysis_directory(directory: Path, reads: Path):
+def create_and_populate_analysis_directory(directory: Path, reads: Path, abc: str):
     try:
         directory.mkdir()
     except OSError as e:
@@ -49,6 +58,16 @@ def create_and_populate_analysis_directory(directory: Path, reads: Path):
     configuration = read_binary("dbspro", CONFIGURATION_FILE_NAME)
     with (directory / CONFIGURATION_FILE_NAME).open("wb") as f:
         f.write(configuration)
+
+    # Write ABC fasta file with ^ prior to sequence (used in cutadapt command)
+    filename_as_string = str(directory) + "/" + ABC_FILE_NAME
+    with dnaio.open(filename_as_string, mode='w', fileformat="fasta") as open_out, \
+            dnaio.open(abc, mode='r', fileformat="fasta") as open_in:
+        for abc in open_in:
+            if not abc.sequence.startswith("^"):
+                abc.sequence = "^" + abc.sequence
+
+            open_out.write(abc)
 
     create_symlink(reads, directory, "reads.fastq.gz")
 
