f21ecb6423ac9ba90a43de36e72ce2969632a037
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-11 10:56:04 +0100

    Change from '' to ""

--- tests/run.sh
+++ tests/run.sh
@@ -3,8 +3,11 @@ set -xoeu pipefail
 
 rm -rf outdir
 
-dbspro run -d outdir -f testdata/reads-10k-DBS.fastq.gz \
-    ../construct-info/handles.tsv ../construct-info/ABC-sequences.fasta
+dbspro init testdata/reads-10k-DBS.fastq.gz outdir
+
+pushd outdir
+
+dbspro run
 
 m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
 test $m == 62ce11e3ac4fb7428a6bf4bfed84f529
