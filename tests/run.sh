8a53bc6844529225865522507a1e95cd07c9483a
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-11 11:37:43 +0100

    Update path to file

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,5 @@ pushd outdir
 
 dbspro run
 
-m=$(cat outdir/umi-counts.txt | sort | md5sum | cut -f 1 -d" ")
+m=$(cat umi-counts.txt | sort | md5sum | cut -f 1 -d" ")
 test $m == 3c31f9634af4238cb0037170104e6d5e
