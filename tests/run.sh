7e0f419630ea90b4230ce18a366c4d3e09d3a1d2
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-11 11:33:04 +0100

    make md5sum independent of order

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,5 @@ pushd outdir
 
 dbspro run
 
-m=$(cat outdir/umi-counts.txt | md5sum | cut -f 1 -d" ")
-test $m == 62ce11e3ac4fb7428a6bf4bfed84f529
+m=$(cat outdir/umi-counts.txt | sort | md5sum | cut -f 1 -d" ")
+test $m == 3c31f9634af4238cb0037170104e6d5e
