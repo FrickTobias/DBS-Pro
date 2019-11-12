cf8b6202b8f9dd3bf9db4ad75b02b4edf57cbd36
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-12 15:04:28 +0100

    Finish fixing merging artifacts

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,5 @@ pushd outdir
 
 dbspro run
 
-m=$(cat outdir/umi_counts.tsv | md5sum | cut -f 1 -d" ")
-test $m == 62ce11e3ac4fb7428a6bf4bfed84f529
+m=$(cat umi_counts.tsv | sort | md5sum | cut -f 1 -d" ")
+test $m == 3c31f9634af4238cb0037170104e6d5e
