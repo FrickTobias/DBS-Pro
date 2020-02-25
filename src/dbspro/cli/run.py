11dda8e06de534d18556544092fc683b0cf8753b
Pontus Hojer <pontushojer@gmail.com>
2020-02-25 16:58:43 +0100

    Refactor `run` add filegraph option.

--- src/dbspro/cli/run.py
+++ src/dbspro/cli/run.py
@@ -16,28 +16,37 @@ class SnakemakeError(Exception):
 
 
 def add_arguments(parser):
+    arg = parser.add_argument
     # Positionals
-    parser.add_argument("targets", nargs="*", metavar="<TARGETS>",
-                        help="File(s) to create excluding paths). If omitted, the full pipeline is run.")
+    arg("targets", nargs="*", metavar="<TARGETS>",
+        help="File(s) to create excluding paths). If omitted, the full pipeline is run.")
     # Options
-    parser.add_argument("-n", "--dryrun", default=False, action="store_true",
-                        help="Perform dry run of pipeline. DEFAULT: False.")
-    parser.add_argument("--dag", default=False, action="store_true",
-                        help="Print the dag in the graphviz dot language. DEFAULT: False. To det output to pdf file, "
-                             "pipe output into dot as follows: '$ dbspro run --dag | dot -Tpdf > dag.pdf'")
-    parser.add_argument("-j", "--cores", "--jobs", metavar="<JOBS>", type=int, default=available_cpu_count(),
-                        help="Maximum number of cores to run in parallel. DEFAULT: Use as many as available.")
-    parser.add_argument('--keepgoing', '-k', default=False, action='store_true',
-                        help='If one job fails, finish the others.')
-    parser.add_argument('--unlock', default=False, action='store_true',
-                        help='Remove a lock on the working directory.')
-    parser.add_argument("--dir", help="Path to analysis directory. DEFAULT: CWD")
+    arg("-n", "--dryrun", default=False, action="store_true",
+        help="Perform dry run of pipeline. DEFAULT: False.")
+    arg("-j", "--cores", "--jobs", metavar="<JOBS>", type=int, default=available_cpu_count(),
+        help="Maximum number of cores to run in parallel. DEFAULT: Use as many as available.")
+    arg('--keepgoing', '-k', default=False, action='store_true',
+        help='If one job fails, finish the others.')
+    arg('--unlock', default=False, action='store_true',
+        help='Remove a lock on the working directory.')
+    arg("--dir", help="Path to analysis directory. DEFAULT: CWD")
+
+    dags = parser.add_mutually_exclusive_group()
+    dags.add_argument(
+        "--dag", default=False, action="store_true",
+        help="Print the dag in the graphviz dot language. DEFAULT: False. To det output to pdf file, "
+             "pipe output into dot as follows: '$ dbspro run --dag | dot -Tpdf > dag.pdf'")
+    dags.add_argument(
+        "--filegraph", default=False, action='store_true',
+        help="Print the file graph showing input/output file from rules in the graphviz dot language (requires "
+             "graphviz to be installed). Default: %(default)s. To get output to pdf file, pipe output into dot "
+             "as follows: blr run --filegraph | dot -Tpdf > filegraph.pdf")
 
 
 def main(args):
     targets = args.targets if args.targets else None
     try:
-        run(args.dryrun, args.cores, args.keepgoing, args.unlock, args.dag, targets)
+        run(args.dryrun, args.cores, args.keepgoing, args.unlock, args.dag, args.filegraph, targets)
     except SnakemakeError:
         sys.exit(1)
     sys.exit(0)
@@ -49,6 +58,7 @@ def run(
         keepgoing: bool = False,
         unlock: bool = False,
         printdag: bool = False,
+        printfilegraph: bool = False,
         targets=None,
         workdir=None,
 ):
@@ -61,6 +71,7 @@ def run(
                         snakemakepath="snakemake",  # Needed in snakemake 3.9.0
                         dryrun=dryrun,
                         printdag=printdag,
+                        printfilegraph=printfilegraph,
                         quiet=False if not printdag else True,
                         cores=cores,
                         keepgoing=keepgoing,
