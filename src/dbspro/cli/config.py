91b2f76d80016c343a1670c17f8d0790e644b5f6
Pontus Hojer <pontushojer@gmail.com>
2020-02-26 11:45:42 +0100

    Delete `set` script. Update configs to enable showing construct layout

--- src/dbspro/cli/config.py
+++ src/dbspro/cli/config.py
@@ -10,9 +10,11 @@ from ruamel.yaml import YAML
 from snakemake.utils import validate
 import pkg_resources
 
+from dbspro.utils import get_abcs
 logger = logging.getLogger(__name__)
 DEFAULT_PATH = "dbspro.yaml"
 SCHEMA_FILE = "config.schema.yaml"
+SEQUENCE_KEYS = {"h1", "h2", "h3"}
 
 
 def main(args):
@@ -21,10 +23,49 @@ def main(args):
 
     if args.set:
         change_config(args.file, args.set)
+    elif args.print_construct:
+        print_construct(args.file)
     else:
-        configs, yaml = load_yaml(args.file)
-        print(f"--- CONFIGS IN: {args.file} ---")
-        yaml.dump(configs, stream=sys.stdout)
+        print_config(args.file)
+
+
+def print_config(file):
+    configs, yaml = load_yaml(file)
+    print(f"--- CONFIGS IN: {file} ---")
+    yaml.dump(configs, stream=sys.stdout)
+
+
+def print_construct(file):
+    configs, yaml = load_yaml(file)
+
+    # Get handles
+    h1 = Handle("H1", configs["h1"])
+    dbs = Handle("DBS", "N"*configs["dbs_len"])
+    h2 = Handle("H2", configs["h2"])
+    umi = Handle("UMI", "N"*configs["umi_len"])
+    abcs = get_abcs(configs["abc_file"])
+    abc = Handle("ABC", "X"*len(abcs["Sequence"][0]))
+    h3 = Handle("H3", configs["h3"])
+
+    handles = [h1, dbs, h2, abc, umi, h3]
+
+    print(
+        f"--- CONSTRUCT LAYOUT (Total length = {sum(h.length for h in handles)}) ---\n"
+        f"Sequence: 5'-{' '.join(h.seq for h in handles)}-3'\n"
+        f"Name:        {' '.join(h.name for h in handles)}\n"
+        f"Size (bp):   {' '.join(h.size for h in handles)}\n"
+    )
+
+
+class Handle:
+    def __init__(self, name, sequence):
+        self.seq = sequence
+        self.length = len(self.seq)
+        self.size = self._add_padding(str(self.length))
+        self.name = self._add_padding(name)
+
+    def _add_padding(self, string: str):
+        return f"|{string.center(self.length-2, ' ')}|"
 
 
 def change_config(filename, changes_set):
@@ -74,3 +115,5 @@ def add_arguments(parser):
                              "write 'null'. Can be given multiple times.")
     parser.add_argument("--file", default=DEFAULT_PATH,
                         help="Configuration file to modify. Default: %(default)s in current directory.")
+    parser.add_argument("-p", "--print-construct", default=False, action="store_true",
+                        help="Show construct layout with current handles inplace.")
