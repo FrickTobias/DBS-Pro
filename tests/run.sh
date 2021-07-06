87dfac18f94b94943ffafe262e8c44bb2bdfefa1
Pontus Hojer <pontushojer@gmail.com>
2021-07-06 10:39:38 +0200

    Add CI workflow for github actions

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xoeu pipefail
 
+if [[ $(uname) == Darwin ]]; then
+  md5="md5 -r"
+else
+  md5=md5sum
+fi
+
 rm -rf outdir
 
 pytest -v tests/
@@ -11,5 +17,5 @@ pushd outdir
 
 dbspro run
 
-m=$(gunzip -c data.tsv.gz | cut -f 1 | sort | md5sum | cut -f 1 -d" ")
+m=$(gunzip -c data.tsv.gz | cut -f 1 | sort | $md5 | cut -f 1 -d" ")
 test $m == 590a6a3d182f88cad7f9404f8b7445db
