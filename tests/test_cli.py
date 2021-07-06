3190d6d992da3c59a038e115cb13dea52bd7dc7c
Pontus Hojer <pontushojer@gmail.com>
2021-07-06 15:11:01 +0200

    Update tests to run md5sum checks in pytest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pytest
+import hashlib
 
+from xopen import xopen
 from dbspro.__main__ import main as dbspro_main
 from dbspro.cli.init import init
 from dbspro.cli.run import run
@@ -8,6 +10,15 @@ from dbspro.cli.config import change_config
 
 TESTDATA_READS = Path("testdata/reads-10k-DBS.fastq.gz")
 ABC_SEQUENCES = Path("testdata/ABC-sequences.fasta")
+
+
+def md5sum(filename):
+    md5_hash = hashlib.md5()
+    with xopen(filename, mode="rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
 
 
 def test_init(tmpdir):
@@ -38,6 +49,9 @@ def test_output_files(tmpdir, targets=["report.ipynb", "data.tsv.gz"]):
     run(targets=targets, workdir=workdir)
     for target in targets:
         assert Path(f"{workdir}/{target}").is_file()
+
+    m = md5sum(workdir / "data.tsv.gz")
+    assert m == "9bef4dfad7df7c09575b1ab6423f1748"
 
 
 def test_version_exit_code_zero():
